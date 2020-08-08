15a647ebd93d9f8d2b4a2b1d7bf6e3baeb8b5316
AVNEETK99 <57584933+AVNEETK99@users.noreply.github.com>
2020-08-08 09:03:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -75,4 +75,59 @@ Display.prototype.feedback = function (customer) {
     const self = this;
     self.hideSubmit();
 
-    
+    setTimeout(function(){
+        feedback.classList.remove('showItem', 'alert', 'alert-success');
+        loading.classList.remove('showItem');
+        self.addCustomer(customer);
+
+    }, 3000);
+};
+
+Display.prototype.addCustomer = function(customer){
+
+    const random = this.getRandom();
+
+    const div = document.createElement('div');
+     div.classList.add('col-11', 'mx-auto', 'col-md-6', 'my-3', 'col-lg-4');
+     div.innerHTML = `<div class="card text-left">
+     <img src="./img/cust-${random}.jpg" class="card-img-top" alt="">
+     <div class="card-body">
+      <!-- customer name -->
+      <h6 class="text-capitalize "><span class="badge badge-warning mr-2">name :</span><span id="customer-name">${customer.name}</span></h6>
+      <!-- end of customer name -->
+      <!-- customer name -->
+      <h6 class="text-capitalize my-3"><span class="badge badge-success mr-2">course :</span><span id="customer-course">
+        ${customer.course}
+       </span></h6>
+      <!-- end of customer name -->
+      <!-- customer name -->
+      <h6 class="text-capitalize"><span class="badge badge-danger mr-2">author :</span><span id="course-author">${customer.author}</span></h6>
+      <!-- end of customer name -->
+     </div>
+    </div>`
+    this.customers.appendChild(div);
+}
+//random number
+Display.prototype.getRandom = function(){
+    let random = Math.floor(Math.random()*5+1);
+    return random;
+};
+
+Display.prototype.clearFields = function(){
+    this.name.value = '';
+    this.course.value = '';
+    this.author.value = '';
+
+    this.name.classList.remove('complete', 'fail');
+    this.course.classList.remove('complete', 'fail');
+    this.author.classList.remove('complete', 'fail');
+};
+
+//customer constructor function
+function Customer(name, course, author){
+    this.name = name;
+    this.course = course;
+    this.author = author;
+};
+
+})()
